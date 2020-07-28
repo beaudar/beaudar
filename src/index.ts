c602316db1252f718d7982775e85a79e221c4da3
zsdycs <mail@lipk.org>
2020-07-28 15:30:30 +0800

    【添加】主页 A 元素 添加 _blank

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,10 @@ import { ConfigurationComponent } from './configuration-component';
 
 document.querySelector('h2#configuration')!
   .insertAdjacentElement('afterend', new ConfigurationComponent().element);
+
+const links = document.getElementsByTagName('a');
+for (let i = 0; i < links.length; i++) {
+  if (/^(https?:)?\/\//.test(links[i].getAttribute('href') as string)) {
+    links[i].target = '_blank';
+  }
+}
