8caef95db2d52ee6436e5d0fa570b241387569bc
Focus Chen <i@focuschen.com>
2022-01-02 17:12:40 +0000

    feat: fix underline inconsistency and update translation

--- src/component/new-comment-component.ts
+++ src/component/new-comment-component.ts
@@ -54,22 +54,22 @@ export class NewCommentComponent {
           </div>
         </div>
         <footer class="new-comment-footer">
-          <a class="text-link markdown-info" target="_blank"
+          <a class="text-link markdown-info no-underline" target="_blank"
              href="https://guides.github.com/features/mastering-markdown/">
-            <svg class="octicon v-align-bottom" viewBox="0 0 16 16" version="1.1"
+            <svg class="octicon v-align-bottom underline" viewBox="0 0 16 16" version="1.1"
               width="16" height="16" aria-hidden="true">
               <path fill-rule="evenodd" d="M14.85 3H1.15C.52 3 0 3.52 0 4.15v7.69C0 12.48.52 13 1.15
                 13h13.69c.64 0 1.15-.52 1.15-1.15v-7.7C16 3.52 15.48 3 14.85 3zM9 11H7V8L5.5 9.92 4
                 8v3H2V5h2l1.5 2L7 5h2v6zm2.99.5L9.5 8H11V5h2v3h1.5l-2.51 3.5z">
               </path>
             </svg>
-            支持 Markdown
+            <span class="underline">支持 Markdown 格式</span>
           </a>
           <button class="btn btn-primary" type="submit" title="发表评论（Ctrl+Enter）">发表评论</button>
           <a class="btn btn-primary" href="${getLoginUrl(
             pageAttrs.url,
           )}" target="_top"><svg class="octicon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>
-          <span class="useGithub">使用 </span>GitHub 登录</a>
+          <span class="useGithub">使用 </span>Github 登录</a>
         </footer>
       </form>`;
 
