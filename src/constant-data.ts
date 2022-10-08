6a4ec6009f89c1d2d468bc543c7defe1005888de
zsdycs <mail@lipk.org>
2022-10-08 19:13:00 +0800

    修改请求地址

--- src/constant-data.ts
+++ src/constant-data.ts
@@ -1,6 +1,6 @@
 import { CommentAuthorAssociation, ReactionID } from './type-declare';
 
-export const BEAUDAR_API = 'https://api.lipk.org';
+export const BEAUDAR_API = 'http://api.lipk.org';
 
 export const PAGE_SIZE = 10;
 
