88bd6575a96e6b06c7e0799a40cc2bc11ddae9f3
zsdycs <mail@lipk.org>
2022-10-08 17:44:01 +0800

    修改 loading 图标样式

--- src/beaudar-loading.ts
+++ src/beaudar-loading.ts
@@ -14,16 +14,36 @@ export const addLoadingStatus = (page: { loading: string }) => {
 
     beaudarBox.innerHTML = `
       <a href="https://beaudar.lipk.org" target="_blank">
-        <div style="position: relative; margin: auto; background-color: rgb(225, 228, 233); border-radius: 50px; width: 100px; height: 100px;">
+        <div style="
+          position: relative;
+          margin: auto;
+          background-color: rgb(225, 228, 233);
+          border-radius: 50px;
+          width: 60px;
+          height: 60px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        ">
           <img
             width="40px"
             height="40px"
-            style="position: absolute; top: 25px; left: 25px; z-index: 2;"
+            style="z-index: 2;"
             src="${BeaudarAvatarUrl}"
             alt="Beaudar(表达)"
             title="Beaudar(表达)"
           />
-          <div style="position: absolute; right: 46px; width: 0px; top: 62px; height: 0px; border-top: 49px solid rgb(225, 228, 233); border-right: 33px solid transparent; border-left: 22px solid transparent; transform: rotate(47deg);"></div>
+          <div style="
+            position: absolute;
+            right: 23px;
+            width: 0px;
+            top: 31px;
+            height: 0px;
+            border-top: 39px solid rgb(225, 228, 233);
+            border-right: 23px solid transparent;
+            border-left: 12px solid transparent;
+            transform: rotate(47deg);
+          "></div>
         </div>
       </a>
       `;
